7b19b46adc7390c7d9ddb5c33468e0e39f3bc136
DESKTOP-FN4R272\WPOSS <cruzdiaz0819@gmail.com>
2023-05-06 11:50:02 -0500

    El historial ya es funcional, debo arreglar un poco su organizacion y ver si lo puedo separar hacia el xml y añadir el numero de la transaccion

--- app/src/main/java/com/example/moviiapk/BaseDeDatos.java
+++ app/src/main/java/com/example/moviiapk/BaseDeDatos.java
@@ -1,6 +1,7 @@
 package com.example.moviiapk;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -16,8 +17,16 @@ public class BaseDeDatos extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE usuario (nombreUsuario TEXT, correoUsuario TEXT," +
                 "numeroUsuario TEXT  primary key,  CantDinero TEXT, contraseñaUsuario TEXT, confirmarUsuario TEXT)");
 
-        db.execSQL("CREATE TABLE transferencias (numeroTransferencia TEXT, confirmarTransferencia TEXT, cantidadTransferencia TEXT)");
+        db.execSQL("CREATE TABLE transferencias (numeroTransferencia TEXT, confirmarTransferencia TEXT, cantidadTransferencia TEXT, fecha TEXT, hora TEXT)");
     }
+
+    public Cursor getTransferencias() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {"numeroTransferencia", "confirmarTransferencia", "cantidadTransferencia", "fecha", "hora"};
+        Cursor cursor = db.query("transferencias", columns, null, null, null, null, null);
+        return cursor;
+    }
+
 
 
     @Override
--- app/src/main/java/com/example/moviiapk/historial.java
+++ app/src/main/java/com/example/moviiapk/historial.java
@@ -2,13 +2,62 @@ package com.example.moviiapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class historial extends AppCompatActivity {
 
+    private LinearLayout linearLayout;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial);
+
+        linearLayout = findViewById(R.id.linear_layout);
+
+        BaseDeDatos baseDeDatos = new BaseDeDatos(this, "admin", null, 1);
+
+        Cursor cursor = baseDeDatos.getTransferencias();
+
+        while (cursor.moveToNext()) {
+            String numeroTransferencia = cursor.getString(0);
+            String confirmarTransferencia = cursor.getString(1);
+            String cantidadTransferencia = cursor.getString(2);
+            String fecha = cursor.getString(3);
+            String hora = cursor.getString(4);
+
+            TextView espacio = new TextView(this);
+            espacio.setText("------------------------");
+            linearLayout.addView(espacio);
+
+            TextView numeroTransferido = new TextView(this);
+            numeroTransferido.setText("Número de la cuenta: " + numeroTransferencia);
+            linearLayout.addView(numeroTransferido);
+
+            TextView transferenciaMonto = new TextView(this);
+            transferenciaMonto.setText("Valor transferido: " + cantidadTransferencia);
+            linearLayout.addView(transferenciaMonto);
+
+            TextView fechaExacta = new TextView(this);
+            fechaExacta.setText("Fecha de la transferencia: " + fecha);
+            linearLayout.addView(fechaExacta);
+
+            TextView horaExacta = new TextView(this);
+            horaExacta.setText("Hora de la transferencia: " + hora);
+            linearLayout.addView(horaExacta);
+
+            TextView espacio2 = new TextView(this);
+            espacio2.setText("------------------------");
+            linearLayout.addView(espacio2);
+        }
+
+        cursor.close();
+
     }
 }
