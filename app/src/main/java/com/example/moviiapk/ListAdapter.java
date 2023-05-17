b19c7aefc57a873fdb5e93f51b13b057dd5d4411
DESKTOP-FN4R272\WPOSS <cruzdiaz0819@gmail.com>
2023-05-17 17:00:39 -0500

    Bien hecha, solo falta arreglar el dinero que no sube de un millon.

--- app/src/main/java/com/example/moviiapk/ListAdapter.java
+++ app/src/main/java/com/example/moviiapk/ListAdapter.java
@@ -16,13 +16,13 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     private LayoutInflater mInflater;
     private Context context;
 
+    private static final int TIPO_TRANSFERENCIA_ENVIADA = 1;
+    private static final int TIPO_TRANSFERENCIA_RECIBIDA = 2;
+
     public ListAdapter(Context context) {
         this.context = context;
         this.mData = new ArrayList<>();
         this.mInflater = LayoutInflater.from(context);
-    }
-
-    public ListAdapter(ArrayList<Object> objects, historial historial) {
     }
 
     @Override
@@ -33,21 +33,27 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
-        if (viewType == 1) {
+        if (viewType == TIPO_TRANSFERENCIA_ENVIADA) {
             view = mInflater.inflate(R.layout.etiqueta, parent, false);
-        } else {
+            return new TransferenciaEnviadaViewHolder(view);
+        } else if (viewType == TIPO_TRANSFERENCIA_RECIBIDA) {
             view = mInflater.inflate(R.layout.etiqueta2, parent, false);
+            return new TransferenciaRecibidaViewHolder(view);
         }
-        return new ViewHolder(view);
+        return null;
     }
+
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         Object item = mData.get(position);
-        if (item instanceof clasecontructor) {
-            holder.bindData((clasecontructor) item);
-        } else if (item instanceof claseConstructor2) {
-            holder.bindData((claseConstructor2) item);
+        if (holder instanceof TransferenciaEnviadaViewHolder && item instanceof clasecontructor) {
+            TransferenciaEnviadaViewHolder transferenciaEnviadaHolder = (TransferenciaEnviadaViewHolder) holder;
+            transferenciaEnviadaHolder.bindData((clasecontructor) item);
+        } else if (holder instanceof TransferenciaRecibidaViewHolder && item instanceof claseConstructor2) {
+            TransferenciaRecibidaViewHolder transferenciaRecibidaHolder = (TransferenciaRecibidaViewHolder) holder;
+            transferenciaRecibidaHolder.bindData((claseConstructor2) item);
+        } else {
         }
     }
 
@@ -55,9 +61,9 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public int getItemViewType(int position) {
         Object item = mData.get(position);
         if (item instanceof clasecontructor) {
-            return 1;
+            return TIPO_TRANSFERENCIA_ENVIADA;
         } else if (item instanceof claseConstructor2) {
-            return 2;
+            return TIPO_TRANSFERENCIA_RECIBIDA;
         }
         return super.getItemViewType(position);
     }
@@ -78,19 +84,20 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView numerotransferencia, cantidadtransferencia, fechatransferencia, horatransfernecia;
-        TextView numeroQEnvio, cantidadQRecibio, fechaQTrans, horaQTrans;
-
         public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class TransferenciaEnviadaViewHolder extends ViewHolder {
+        TextView numerotransferencia, cantidadtransferencia, fechatransferencia, horatransfernecia;
+
+        public TransferenciaEnviadaViewHolder(View itemView) {
             super(itemView);
             numerotransferencia = itemView.findViewById(R.id.nameTextView);
             cantidadtransferencia = itemView.findViewById(R.id.statusTextView);
             fechatransferencia = itemView.findViewById(R.id.cityTextView);
             horatransfernecia = itemView.findViewById(R.id.precioTextView);
-            numeroQEnvio = itemView.findViewById(R.id.nameTextView2);
-            cantidadQRecibio = itemView.findViewById(R.id.statusTextView2);
-            fechaQTrans = itemView.findViewById(R.id.cityTextView2);
-            horaQTrans = itemView.findViewById(R.id.precioTextView2);
         }
 
         void bindData(final clasecontructor item) {
@@ -98,7 +105,18 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             cantidadtransferencia.setText(item.getCantidadtransferencia());
             fechatransferencia.setText(item.getFechatransferencia());
             horatransfernecia.setText(item.getHoratransfernecia());
+        }
+    }
 
+    public class TransferenciaRecibidaViewHolder extends ViewHolder {
+        TextView numeroQEnvio, cantidadQRecibio, fechaQTrans, horaQTrans;
+
+        public TransferenciaRecibidaViewHolder(View itemView) {
+            super(itemView);
+            numeroQEnvio = itemView.findViewById(R.id.nameTextView2);
+            cantidadQRecibio = itemView.findViewById(R.id.statusTextView2);
+            fechaQTrans = itemView.findViewById(R.id.cityTextView2);
+            horaQTrans = itemView.findViewById(R.id.precioTextView2);
         }
 
         void bindData(final claseConstructor2 item) {
@@ -106,7 +124,12 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             cantidadQRecibio.setText(item.getcantidadQRecibio());
             fechaQTrans.setText(item.getfechaQTrans());
             horaQTrans.setText(item.gethoraQTrans());
+        }
+    }
 
+    public class DefaultViewHolder extends ViewHolder {
+        public DefaultViewHolder(View itemView) {
+            super(itemView);
         }
     }
 }
